6ef7b7e20694f269bb33e0d93ba434ad90bd79e6
Hemanth Krishna <hkpdev008@gmail.com>
2022-07-26 01:52:27 +0530

    lint: add lints and fixes
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

--- test/middleware.ts
+++ test/middleware.ts
@@ -1,33 +1,31 @@
+/* eslint-disable no-console */
 import { Melonpan, MelonRouter } from "../index";
 
 let counter = 0;
 const melonpan = new Melonpan();
-melonpan.middleware((req, res, next) => {
-  counter++;
+melonpan.middleware((_req, _res, next) => {
+  counter += 1;
   next();
 });
-melonpan.middleware((req, res, next) => {
-  counter++;
+melonpan.middleware((_req, _res, next) => {
+  counter += 1;
   next();
 });
-melonpan.get("/hello", () => {
-  return new Response(`this is a nested Router ${counter}`);
-});
-melonpan.middleware((req, res, next) => {
+melonpan.get(
+  "/hello",
+  () => new Response(`this is a nested Router ${counter}`)
+);
+melonpan.middleware(() => {
   console.log("middlware has been hit 3");
 });
 
-melonpan.get("/hi", () => {
-  return new Response("3 middlewares have been called");
-});
+melonpan.get("/hi", () => new Response("3 middlewares have been called"));
 
 const router = new MelonRouter();
-router.middleware((req, res, next) => {
+router.middleware(() => {
   console.log("router: middleware");
 });
-router.get("/mid", () => {
-  return new Response("Router hit1");
-});
+router.get("/mid", () => new Response("Router hit1"));
 melonpan.use("/router", router);
 export default {
   port: 3000,
